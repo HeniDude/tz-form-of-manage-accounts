429e42662007cd51beb3173b0be1c582eec5e880
HeniDude <patrol20032003@gmail.com>
2026-02-03 14:07:02 +0300

    feat(accounts): initial implementation

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,7 +17,6 @@ module.exports = {
     'prettier',
   ],
   rules: {
-    // минимальные, без шума
     'vue/multi-word-component-names': 'off',
     '@typescript-eslint/no-unused-vars': ['warn'],
     '@typescript-eslint/no-explicit-any': 'warn',
